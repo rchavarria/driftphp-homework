de7722960af9bfd3d99cda4e4cf537cfa9867451
Rubén Chavarría <txingo@gmail.com>
2020-08-05 18:51:42 +0200

    Primer test, ayuda a configurar PHPUnit en PhpStorm

---
+++ test/HookTest.php
@@ -0,0 +1,11 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+
+class HookTest extends TestCase
+{
+    public function testHook()
+    {
+        $this->assertTrue(false, 'This is just a failing test');
+    }
+}
