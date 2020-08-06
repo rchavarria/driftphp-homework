6396a6b129630a93cd0bf5d02321c21c6fc59bd3
Rubén Chavarría <txingo@gmail.com>
2020-08-06 18:10:05 +0200

    Implementa método find del Repositorio DBAL

--- src/DBAL/Model/User/DBALUserRepository.php
+++ src/DBAL/Model/User/DBALUserRepository.php
@@ -2,9 +2,11 @@
 
 namespace Infrastructure\DBAL\Model\User;
 
+use App\Domain\Model\User\UserNotFoundException;
 use Domain\Model\User\User;
 use Domain\Model\User\UserRepository;
 use Drift\DBAL\Connection;
+use Drift\DBAL\Result;
 use React\Promise\PromiseInterface;
 
 class DBALUserRepository implements UserRepository
@@ -19,7 +21,28 @@ class DBALUserRepository implements UserRepository
 
     public function find(string $uid): PromiseInterface
     {
-        // TODO: Implement find() method.
+        $queryBuilder = $this
+            ->connection
+            ->createQueryBuilder()
+            ->select('*')
+            ->from('users')
+            ->where('uid = ?')
+            ->setParameters([$uid]);
+
+        return $this
+            ->connection
+            ->query($queryBuilder)
+            ->then(function (Result $result) {
+                $userAsArray = $result->fetchFirstRow();
+                if (is_null($userAsArray)) {
+                    throw new UserNotFoundException();
+                }
+
+                return new User(
+                    $userAsArray['uid'],
+                    $userAsArray['name']
+                );
+            });
     }
 
     public function save(User $user): PromiseInterface
