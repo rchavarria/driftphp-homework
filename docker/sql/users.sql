75e520908f8214060d580fbc7f5a6c3f2fa882d2
Rubén Chavarría <txingo@gmail.com>
2020-08-06 18:28:02 +0200

    Añade el SQL necesario para crear la base de datos y la tabla de usuarios

---
+++ docker/sql/users.sql
@@ -0,0 +1,10 @@
+--
+-- Create database and table for Users
+--
+create database usersdb;
+use usersdb;
+create table users
+(
+    uid  varchar(64) default '' not null primary key,
+    name varchar(255)           not null
+);
