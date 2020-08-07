c5005580588417b87b11951d90b95101e1d25c6d
Rubén Chavarría <txingo@gmail.com>
2020-08-07 13:53:56 +0200

    Activa el bundle del Event Bus, el bundle de AMQP no es necesario

--- Drift/config/bundles.php
+++ Drift/config/bundles.php
@@ -15,6 +15,7 @@ declare(strict_types=1);
 
 return [
     Symfony\Bundle\FrameworkBundle\FrameworkBundle::class => ['all' => true],
-    Drift\CommandBus\CommandBusBundle::class => ['all' => true],
     Drift\DBAL\DBALBundle::class => ['all' => true],
+    Drift\CommandBus\CommandBusBundle::class => ['all' => true],
+    Drift\EventBus\EventBusBundle::class => ['all' => true],
 ];
