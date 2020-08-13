c2cbbc34d83c789b3decf650f03058d1eb1c2ed8
Rubén Chavarría <txingo@gmail.com>
2020-08-13 10:45:15 +0200

    Escuchar eventos de que se eliminar un usuario y enviarlos a todas las conexiones de Websockets

--- src/Domain/EventSubscriber/BroadcastDomainEvents.php
+++ src/Domain/EventSubscriber/BroadcastDomainEvents.php
@@ -2,6 +2,7 @@
 
 namespace Domain\EventSubscriber;
 
+use App\Domain\Event\UserWasDeleted;
 use Domain\Event\UserWasSaved;
 use Drift\HttpKernel\Event\DomainEventEnvelope;
 use Drift\Websocket\Connection\Connections;
@@ -22,7 +23,7 @@ class BroadcastDomainEvents implements EventSubscriberInterface
         $this->connections = $eventsConnections;
     }
 
-    public final function broadcastUserWasCreated(DomainEventEnvelope $eventEnvelope): void
+    public final function broadcastUserWasSaved(DomainEventEnvelope $eventEnvelope): void
     {
         /** @var UserWasSaved $event */
         $event = $eventEnvelope->getDomainEvent();
@@ -33,9 +34,25 @@ class BroadcastDomainEvents implements EventSubscriberInterface
         $this
             ->connections
             ->broadcast(json_encode([
-                'type' => 'user was created',
+                'type' => get_class($event),
                 'uid' => $uid,
                 'name' => $event->getUser()->getName()
+            ]));
+    }
+
+    public final function broadcastUserWasDeleted(DomainEventEnvelope $eventEnvelope): void
+    {
+        /** @var UserWasDeleted $event */
+        $event = $eventEnvelope->getDomainEvent();
+
+        $uid = $event->getUid();
+        echo '* broadcast user was deleted: ', $uid, PHP_EOL;
+
+        $this
+            ->connections
+            ->broadcast(json_encode([
+                'type' => get_class($event),
+                'uid' => $uid
             ]));
     }
 
@@ -43,7 +60,10 @@ class BroadcastDomainEvents implements EventSubscriberInterface
     {
         return [
             UserWasSaved::class => [
-                ['broadcastUserWasCreated', 0]
+                ['broadcastUserWasSaved', 0]
+            ],
+            UserWasDeleted::class => [
+                ['broadcastUserWasDeleted', 0]
             ]
         ];
     }
