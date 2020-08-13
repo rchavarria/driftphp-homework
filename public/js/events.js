c9f7f33a732926647e456ea53d8e1450bbff42b6
Rubén Chavarría <txingo@gmail.com>
2020-08-13 17:30:49 +0200

    Añade un cliente de Websockets que escucha todos los eventos y reacciona a ellos
    - Guarda un registro de todos los eventos que ocurren
    - Actualiza la lista de los usuarios

---
+++ public/js/events.js
@@ -0,0 +1,62 @@
+(function () {
+    'use strict';
+
+    //
+    // configuration
+    //
+    const wsUrl = 'ws://localhost:9009/events';
+
+    //
+    // get element to manipulate
+    //
+    const $users = document.getElementById('users');
+    const $events = document.getElementById('events');
+
+    function onAnyEvent(event) {
+        const li = document.createElement('li');
+        li.setAttribute('class', 'list-group-item');
+        li.innerHTML = event.type;
+
+        $events.prepend(li);
+    }
+
+    function onUserWasSaved(event) {
+        const li = document.createElement('li');
+        li.setAttribute('id', event.uid);
+        li.setAttribute('class', 'list-group-item');
+        li.innerHTML = event.uid + ': ' + event.name;
+
+        $users.append(li);
+    }
+
+    function onUserWasDeleted(event) {
+        const li = document.getElementById(event.uid);
+        li.remove();
+    }
+
+    function onWsMessage(messageEvent) {
+        const event = JSON.parse(messageEvent.data);
+        onAnyEvent(event);
+
+        switch (event.type) {
+            case 'Domain\\Event\\UserWasSaved':
+                onUserWasSaved(event);
+                break;
+            case 'Domain\\Event\\UserWasDeleted':
+                onUserWasDeleted(event);
+                break;
+        }
+    }
+
+    function init() {
+        const ws = new WebSocket(wsUrl);
+        ws.onmessage = onWsMessage
+    }
+
+    //
+    // start!
+    //
+
+    init();
+
+}());
