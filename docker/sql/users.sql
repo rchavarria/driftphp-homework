4ddab3c6e3d9d447a44e12386d7fee197a154faa
Rubén Chavarría <txingo@gmail.com>
2020-08-14 17:48:54 +0200

    Crea un usuario como datos iniciales en la base de datos

--- docker/sql/users.sql
+++ docker/sql/users.sql
@@ -2,9 +2,14 @@
 -- Create database and table for Users
 --
 create database usersdb;
+
 use usersdb;
+
 create table users
 (
     uid  varchar(64) default '' not null primary key,
     name varchar(255)           not null
 );
+
+insert into users (uid, name)
+values ('plnctn', 'Plancton, el origen');
